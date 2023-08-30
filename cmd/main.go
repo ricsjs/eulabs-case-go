a8adac0c0a902ead5171a42a44c82bf9a993c680
Ricardo Alencar <ricfilho0007@gmail.com>
2023-08-29 22:37:50 -0300

    separação das rotas em outro arquivo e finalização

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	handler "eulabs-case-go/api/Handler"
+	"eulabs-case-go/routes"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/produtos", handler.GetAll)
-	e.GET("/produtos/:id", handler.GetProduto)
-	e.POST("/produtos", handler.PostProdutos)
-	e.PUT("/produtos/:id", handler.PutProduto)
-	e.DELETE("produtos/:id", handler.DeleteProduto)
+	routes.SetupRoutes(e)
 	e.Logger.Fatal(e.Start(":3000"))
 }
---
+++ routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	handler "eulabs-case-go/api/Handler"
+
+	"github.com/labstack/echo"
+)
+
+func SetupRoutes(e *echo.Echo) {
+	e.GET("/produtos", handler.GetAll)
+	e.GET("/produtos/:id", handler.GetProduto)
+	e.POST("/produtos", handler.PostProdutos)
+	e.PUT("/produtos/:id", handler.PutProduto)
+	e.DELETE("/produtos/:id", handler.DeleteProduto)
+}
